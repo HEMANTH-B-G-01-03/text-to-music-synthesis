0db33ba7e05a0f0fb9e27c437e486847824474f2
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-06-09 10:49:11 +0530

    feat: integrate local MusicGen audio generation

--- backend/music_generator.py
+++ backend/music_generator.py
@@ -1,32 +1,71 @@
+# import os
+# import requests
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# HF_TOKEN = os.getenv("HF_TOKEN")
+
+# API_URL = "https://router.huggingface.co/fal-ai/fal-ai/musicgen-small"
+
+# HEADERS = {
+#     "Authorization": f"Bearer {HF_TOKEN}",
+#     "Content-Type": "application/json"
+# }
+
+
+# def generate_music(prompt):
+
+#     payload = {
+#         "prompt": prompt
+#     }
+
+#     response = requests.post(
+#         API_URL,
+#         headers=HEADERS,
+#         json=payload
+#     )
+
+#     print(response.status_code)
+#     print(response.text)
+
+#     return response.json()
+
+
+from transformers import pipeline
+import scipy
+import uuid
 import os
-import requests
-from dotenv import load_dotenv
 
-load_dotenv()
+print("Loading MusicGen...")
 
-HF_TOKEN = os.getenv("HF_TOKEN")
+pipe = pipeline(
+    "text-to-audio",
+    model="facebook/musicgen-small",
+    device=-1
+)
 
-API_URL = "https://router.huggingface.co/fal-ai/fal-ai/musicgen-small"
-
-HEADERS = {
-    "Authorization": f"Bearer {HF_TOKEN}",
-    "Content-Type": "application/json"
-}
+print("MusicGen loaded successfully!")
 
 
 def generate_music(prompt):
 
-    payload = {
-        "prompt": prompt
-    }
+    os.makedirs("outputs", exist_ok=True)
 
-    response = requests.post(
-        API_URL,
-        headers=HEADERS,
-        json=payload
+    music = pipe(
+        prompt,
+        forward_params={
+            "max_new_tokens": 256
+        }
     )
 
-    print(response.status_code)
-    print(response.text)
+    filename = f"{uuid.uuid4()}.wav"
+    filepath = os.path.join("outputs", filename)
 
-    return response.json()
+    scipy.io.wavfile.write(
+        filepath,
+        rate=music["sampling_rate"],
+        data=music["audio"]
+    )
+
+    return filepath
