744b8d189115e92a70165a4b77d42bde67981de4
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-06-09 10:21:41 +0530

    updated main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,39 @@
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+# from backend.prompt_enhancer import enhance_prompt
+
+# app = FastAPI()
+
+
+# class PromptRequest(BaseModel):
+#     prompt: str
+
+
+# @app.get("/")
+# def home():
+#     return {
+#         "project": "Text To Music Synthesis",
+#         "status": "running"
+#     }
+
+
+# @app.post("/enhance")
+# def enhance(req: PromptRequest):
+
+#     enhanced = enhance_prompt(req.prompt)
+
+#     return {
+#         "original_prompt": req.prompt,
+#         "enhanced_prompt": enhanced
+#     }
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 from backend.prompt_enhancer import enhance_prompt
+from backend.music_generator import generate_music
 
 app = FastAPI()
 
@@ -18,12 +50,15 @@ def home():
     }
 
 
-@app.post("/enhance")
-def enhance(req: PromptRequest):
+@app.post("/generate")
+def generate(req: PromptRequest):
 
-    enhanced = enhance_prompt(req.prompt)
+    enhanced_prompt = enhance_prompt(req.prompt)
+
+    audio_file = generate_music(enhanced_prompt)
 
     return {
         "original_prompt": req.prompt,
-        "enhanced_prompt": enhanced
+        "enhanced_prompt": enhanced_prompt,
+        "audio_file": audio_file
     }
