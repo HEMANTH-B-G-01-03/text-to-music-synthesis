9d91cf5e8a786cb8c4a3cc49347286197580d545
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2026-06-19 13:45:44 +0530

    Update AudioPlayer component

--- frontend/src/components/AudioPlayer.jsx
+++ frontend/src/components/AudioPlayer.jsx
@@ -1,22 +1,45 @@
+// function AudioPlayer({ audioUrl }) {
+//   return (
+//     <div>
+//       <h3>Generated Music</h3>
+
+//       <audio
+//         controls
+//         style={{ width: "100%" }}
+//         src={`http://127.0.0.1:8000${audioUrl}`}
+//       />
+
+//       <br />
+//       <br />
+
+//       <a
+//         href={`http://127.0.0.1:8000${audioUrl}`}
+//         download
+//       >
+//         Download Music
+//       </a>
+//     </div>
+//   );
+// }
+
+// export default AudioPlayer;
+
+
 function AudioPlayer({ audioUrl }) {
   return (
-    <div>
-      <h3>Generated Music</h3>
-
+    <div className="audio-section">
       <audio
         controls
-        style={{ width: "100%" }}
         src={`http://127.0.0.1:8000${audioUrl}`}
+        className="audio-player"
       />
-
-      <br />
-      <br />
 
       <a
         href={`http://127.0.0.1:8000${audioUrl}`}
         download
+        className="download-btn"
       >
-        Download Music
+        ⬇ Download Music
       </a>
     </div>
   );
